748235f29e95f9687f544d40fe68f5e311ebe48b
Alena <Alena1118@icloud.com>
2025-10-22 19:47:54 +0700

    удаление тасок

---
+++ src/components/DeleteIcon/DeleteIcon.tsx
@@ -0,0 +1,25 @@
+import styles from './DeleteIcon.module.scss';
+
+type Props = {
+  onClick: () => void;
+};
+
+export const DeleteIcon = ({ onClick }: Props) => {
+  return (
+    <button type="button" onClick={onClick} className={styles.deleteButton}>
+      <svg
+        width="16"
+        height="16"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+      >
+        <polyline points="3,6 5,6 21,6"></polyline>
+        <path d="m19,6v14a2,2 0 0,1 -2,2H7a2,2 0 0,1 -2,-2V6m3,0V4a2,2 0 0,1 2,-2h4a2,2 0 0,1 2,2v2"></path>
+        <line x1="10" y1="11" x2="10" y2="17"></line>
+        <line x1="14" y1="11" x2="14" y2="17"></line>
+      </svg>
+    </button>
+  );
+};
